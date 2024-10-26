bdd1cb02848fa6ce066c21e6010f69d245b2a99e
Aditya <Adityapandeyadp@gmail.com>
2024-10-26 21:37:03 +0530

    good for now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Endsolver from './End_solver';
+import Enduser from './End_user';
+import Manageuser from './Manage_user';
+import Navbar from './Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Navbar/>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path='/end-solver' element={<Endsolver/>} />
+        <Route path='/enduser' element={<Enduser/>} />
+        <Route path='/manager' element={<Manageuser/>} />
+    </Routes>
+    </>
   )
 }
 
