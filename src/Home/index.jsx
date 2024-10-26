6c758e14cb65052afd02de0af3bc2f2ce89e41d5
Aditya <Adityapandeyadp@gmail.com>
2024-10-27 01:55:37 +0530

    forna foiid

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,28 +1,27 @@
-<<<<<<< HEAD
-import React from 'react'
-// impoty
-=======
+
 import React from "react";
 import "./index.scss";
->>>>>>> c5f0dfb (okk for now)
 
 const Home = () => {
   return (
     <div className="container">
       <h1 className="heading">Complaint Management System</h1>
       <div className="options">
+        <a href="/">
         <div className="option-card">
           <h2 className="option-title">File a Complaint</h2>
           <p className="option-text">
             Start by submitting your complaint here. Our team will review it and get back to you as soon as possible.
           </p>
-        </div>
+          </div>
+        </a>
+        <a href="/">
         <div className="option-card">
           <h2 className="option-title">Track Complaint Status</h2>
           <p className="option-text">
             Already submitted a complaint? Check its status here to see if there are any updates.
           </p>
-        </div>
+        </div></a>
       </div>
     </div>
   );
