db7a87b50de3d6425582c82c1676525c99ee8099
Chandan Reddy <chandansdreddy@gamil.com>
2024-10-27 18:38:59 +0530

    g tekhg kjtnghje

--- src/End_user/index.jsx
+++ src/End_user/index.jsx
@@ -6,71 +6,6 @@ const formatDate = (dateString) => {
   return new Date(dateString).toLocaleDateString(undefined, options);
 };
 
-<<<<<<< HEAD
-  // List of main options
-  const mainOptions = [
-    { label: "Complaint", value: "complaint" },
-    { label: "Catering", value: "Catering" },
-    { label: "Security", value: "security" },
-    { label: "EnquiryCounter", value: "EnquiryCounter" },
-    { label: "DirtyLinen", value: "DirtyLinen" },
-    { label: "SeatingAssignment", value: "SeatingAssignment" },
-    { label: "Powersupply", value: "Powersupply" },
-  ];
-
-  // Sub-options based on main selection
-  const subOptions = {
-    Catering: ["hygiene", "quality or taste", "Delay"],
-    Hygiene: ["washroom", "compartment"],
-    security: ["misbehaviour", "missing of things"],
-    EnquiryCounter: ["harsh behaviour"],
-    DirtyLinen: ["no pillow,bedsheet provided", "replace/not clean"],
-    SeatingAssignment: [
-      "Problem with co-passengers",
-      "seat occupied by unknown",
-    ],
-    Powersupply: ["plug point", "ac/fan not working", "short circuit"],
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Store form data in state to trigger API call in useEffect
-    setFormData({
-      pnr: pnr,
-      content: complaint + " " + station + " " + subStation,
-      title: station,
-      contact: contact,
-    });
-  };
-
-  useEffect(() => {
-    if (formData) {
-      // Perform API call with form data
-      const submitComplaint = async () => {
-        try {
-          const res = await fetch(
-            "https://backend-rail-cms-production.up.railway.app/api/add",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(formData),
-            }
-          );
-          const data = await res.json();
-          setResponse(data);
-          if (data.pnr === pnr) {
-            alert("Complaint submitted successfully!");
-            setFormData(null);
-            setResponse(null);
-            window.location.reload();
-          }
-        } catch (error) {
-          console.error("Error submitting complaint:", error);
-        }
-      };
-=======
 const Endsolver = () => {
   const [data, setData] = useState([]);
   const [sortByPriority, setSortByPriority] = useState(false);
@@ -88,7 +23,6 @@ const Endsolver = () => {
         setData(parsedData.reverse());
       });
   }, []);
->>>>>>> 3e855a4e02f1e6861639d6b54dc3f44f4e3feb4f
 
   const handleSortByPriorityToggle = () => {
     setSortByPriority(!sortByPriority);
